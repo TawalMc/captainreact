5f1abedc781e51a46e008fc15105a444f3923cdf
TawalMc <developtawaliou@gmail.com>
2020-09-30 20:29:16 +0100

    Modify README.md and package.json for the current update and deployment.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 const cors = require("cors");
 const retrieveData = require("./retrieveData");
 
@@ -15,6 +16,14 @@ app.post("/api/heroAPI", (req, res) => {
     retrieveData(req.body.post, res);
 })
 
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+    })
+}
+
 app.listen(port, () => {
-    console.log(`Listening on port: ${port}`);
+    /* console.log(`Listening on port: ${port}`) */;
 });
